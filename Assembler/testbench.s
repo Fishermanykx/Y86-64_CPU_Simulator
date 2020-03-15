c5a01c66c207b960c0a63a64b51070c993e5f859
fishermanykx <fishermanykx.mail@gmail.com>
2020-03-15 11:25:19 +0800

    修正了testbench.s

--- Assembler/testbench.s
+++ Assembler/testbench.s
@@ -1,9 +1,9 @@
 000 sum
 001   xorq rax rax 
-002   irmovq 1 rbx  
-003   irmovq 1 rdx  
-004   irmovq 0x65 rcx 
-005   irmovq 0x65 rsi
+002   irmovq $1 rbx  
+003   irmovq $1 rdx  
+004   irmovq $0x65 rcx 
+005   irmovq $0x65 rsi
 006   loop
 007     addq rbx rax 
 008     addq rdx rbx 
