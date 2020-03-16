4b0cc767968f86b5fa74f342a444dc5ad9228add
fishermanykx <fishermanykx.mail@gmail.com>
2020-03-16 17:17:47 +0800

    完成__init__

--- cpu_simulator/Y86_simulator.py
+++ cpu_simulator/Y86_simulator.py
@@ -2,28 +2,45 @@
 @Description: 
 @Author: Fishermanykx
 @LastEditors: Fishermanykx
-@LastEditTime: 2020-03-15 23:58:34
+@LastEditTime: 2020-03-16 17:17:29
 '''
+from pprint import pprint
 
 
 class CPUSimulator:
 
   def __init__(self, filename):
     "载入含机器码的文件"
-    self.regFile = [0] * 8  # 寄存器文件，编号依次为0~7
+    # 初始化寄存器文件
+    self.regFile = {}  # 寄存器文件，编号依次为0~7
+    for i in range(8):
+      self.regFile[i] = 0
+    # 初始化内存
     self.memory = [0] * 1000  # 内存
-    self.rip = 0
+    self.rip = 0  # instruction pointer
+    # 构建地址-代码表
+    self.codes = {}
     inf = open(filename + ".bin", "r")
     while True:
       line = inf.readline()
       if line == '':
         break
       line = line.split()
-      address = int(line[0])
-
+      address = line[0]
+      self.codes[address] = line[1]
     inf.close()
+
+    # pprint(self.codes)
+
+    def main_process(self):
+      pass
 
 
 if __name__ == "__main__":
-  simulator = CPUSimulator("testbench")
+  simulator = CPUSimulator(
+      "D:/Materials_Study/Computer_Science/Computer_Architecture/Exercises/Y86-64_CPU_Simulator/cpu_simulator/testbench"
+  )  # Windows
+  # simulator = CPUSimulator(
+  #     "/media/fisher/DATA/Materials_Study/Computer_Science/Computer_Architecture/Exercises/Y86-64_CPU_Simulator/cpu_simulator/testbench"
+  # )  # Linux
   print(simulator.regFile[0])  # 结果的值在rax里
